83563875d092c9af4c71ef45463cbac854b7ff3d
Eva261002 <eviitamariavargas@gmail.com>
2024-09-19 21:02:43 -0400

    Update prestamo_libros.php
    Botón atrás

--- prestamo_libros.php
+++ prestamo_libros.php
@@ -94,6 +94,11 @@ if (isset($_POST['accion'])) {
 </head>
 <body class="bg-gray-100">
     <div class="container mx-auto py-12">
+
+<!-- Botón-->
+    <div class="flex justify-start mb-6">
+            <a href="catalogo_libros.php" class="bg-gray-500 text-white px-4 py-2 rounded-md hover:bg-gray-600">Volver</a>
+        </div>
         <h1 class="text-3xl font-bold text-center mb-6">Préstamo de Libros</h1>
 
         <?php if ($id_libro !== null): ?>
